ebee424c5413c6c8f681f4801e67870dcb97426a
AliciaMstt <ticatla@gmail.com>
2019-02-05 11:13:44 -0600

    added maize plink examples

---
+++ Unidad5/Prac_Uni5/maices/bin/ejerciciomaices.R
@@ -0,0 +1,7 @@
+#### Este script carga la base de datos maizteocinte_SNO50l_meta_extended
+
+
+### cargar archivos de datos
+getwd() # el resultado de correr este comando debe ser la ruta a /Unidad7/Prac_Uni7/mantel/bin 
+maices<-read.delim(file="maizteocintle_SNP50k_meta_extended.txt")
+
